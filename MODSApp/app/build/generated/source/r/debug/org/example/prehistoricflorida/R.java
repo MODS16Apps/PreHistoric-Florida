b5d3712edc74610fcb486f8cecaaa3e14b310a84
Jonathanerskine <jerskine2016@fau.edu>
2016-07-19 18:53:07 -0400

    v0.5.2.2
    added button for Animal Info
    added button Size Comparison
    added button for Evolution
    added button for AR

--- MODSApp/app/build/generated/source/r/debug/org/example/prehistoricflorida/R.java
+++ MODSApp/app/build/generated/source/r/debug/org/example/prehistoricflorida/R.java
@@ -14,17 +14,21 @@ public final class R {
         public static final int background=0x7f040000;
     }
     public static final class drawable {
-        public static final int baseum=0x7f020000;
-        public static final int bones=0x7f020001;
-        public static final int footprint=0x7f020002;
-        public static final int games=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int map=0x7f020005;
-        public static final int megalodonlogo=0x7f020006;
-        public static final int scavenger_hunt=0x7f020007;
-        public static final int sealevel=0x7f020008;
-        public static final int sealevelchanging=0x7f020009;
-        public static final int trivia=0x7f02000a;
+        public static final int animalinfo=0x7f020000;
+        public static final int ar=0x7f020001;
+        public static final int baseum=0x7f020002;
+        public static final int bones=0x7f020003;
+        public static final int evolution=0x7f020004;
+        public static final int footprint=0x7f020005;
+        public static final int games=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int map=0x7f020008;
+        public static final int megalodonlogo=0x7f020009;
+        public static final int scavenger_hunt=0x7f02000a;
+        public static final int sealevel=0x7f02000b;
+        public static final int sealevelchanging=0x7f02000c;
+        public static final int sizecomp=0x7f02000d;
+        public static final int trivia=0x7f02000e;
     }
     public static final class id {
         public static final int a1=0x7f06000e;
