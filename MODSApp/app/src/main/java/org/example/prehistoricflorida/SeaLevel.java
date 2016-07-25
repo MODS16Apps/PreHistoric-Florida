f71532e87b45bdf8ae46e1af456a452f9d7c6cf5
jacob <frankj2016@fau.edu>
2016-07-25 15:12:08 -0400

    v0.7.6.1

--- MODSApp/app/src/main/java/org/example/prehistoricflorida/SeaLevel.java
+++ MODSApp/app/src/main/java/org/example/prehistoricflorida/SeaLevel.java
@@ -1,34 +1,31 @@
-/***
- * Excerpted from "Hello, Android",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/eband3 for more book information.
- ***/
 package org.example.prehistoricflorida;
 
+/**
+ * Created by Marcus on 7/10/2015.
+ */
+
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.widget.VideoView;
 
 public class SeaLevel extends Activity {
-    private static final String TAG = "PrehistoricFlorida";
-
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sealevel);
+
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView1);
+
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sea_level_changing); //do not add any extension
+        videoView.setVideoURI(video);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 }
